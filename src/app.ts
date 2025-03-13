baed03392834908b8a37f6dde08ff46e66f180e4
🎲 <dice-a6db@outlook.com>
2025-03-13 23:15:01 +0000

    fix: remove caching middleware from /mods/* route to streamline request handling as its triggering deno caching which cannot be invalidated

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,12 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
-import { cache } from "hono/cache";
 import { trustedClient } from "./middleware/trustedClient.ts";
 
 export const app = new OpenAPIHono();
 app.use(logger());
 app.use("/*", cors());
 app.use("/*", trustedClient());
-app.use(
-  "/mods/*",
-  cache({
-    cacheName: "dcs-dropzone",
-    cacheControl: "public, max-age=86400, s-maxage=86400",
-    wait: true,
-  }),
-);
 
 app.doc("/v3/api-docs", {
   openapi: "3.0.3",
