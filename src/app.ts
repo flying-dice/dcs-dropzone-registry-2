0cbf5e115dee6a6ee2bdd88bab10561894b20623
🎲 <dice-a6db@outlook.com>
2025-03-13 20:12:36 +0000

    feat: add caching middleware to enhance performance and reduce load times

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
+import { cache } from "hono/cache";
 import { trustedClient } from "./middleware/trustedClient.ts";
 
 export const app = new OpenAPIHono();
 app.use(logger());
 app.use("/*", cors());
 app.use("/*", trustedClient());
+app.use(
+  "/*",
+  cache({
+    cacheName: "dcs-dropzone",
+    cacheControl: "max-age=86400",
+    wait: true,
+  }),
+);
 
 app.doc("/v3/api-docs", {
   openapi: "3.0.3",
